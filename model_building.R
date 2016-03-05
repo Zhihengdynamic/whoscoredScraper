f11f23c0557c738f167894c08292d45296919245
johnlocker <jonathan.klaiber@gmail.com>
2016-03-05 14:48:26 +0100

    R file to build index model

---
+++ model_building.R
@@ -0,0 +1,11 @@
+# Master Thesis
+# Jonathan Klaiber
+# created 05.03.2016
+# last modified: 05.03.2016
+
+data <- read.csv('player_stats_validated.csv', encoding = 'UTF-8', 
+                 stringsAsFactors = FALSE)
+
+
+# Develop the different sub indices
+
